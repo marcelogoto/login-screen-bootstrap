9e02cf763befc060fd21ed7cf13b3cb40abf4f93
Marcelo Goto <goto.marcelo.15@gmail.com>
2018-05-15 14:28:04 -0300

    DONE: Added javascript code for validations

---
+++ script.js
@@ -0,0 +1,54 @@
+var numberOfAttempts = 3;
+
+function validateForm() {
+    
+    var name = document.getElementById("username").value;
+    
+    var password = document.getElementById("password").value;
+    
+    if (name == "") {
+        numberOfAttempts--;
+        
+        if(numberOfAttempts== 0){
+            displayMaximumAttemptsMessageAndLockSendButton();
+            return false;
+        }
+        
+        alert("Name must be filled out. You have "+numberOfAttempts+" attempt(s) left" );
+    }
+    else if(password == ""){
+        numberOfAttempts--;
+        
+        if(numberOfAttempts == 0){
+            displayMaximumAttemptsMessageAndLockSendButton();
+            return false;
+        } 
+        
+        alert("Password must be filled out. You have "+numberOfAttempts+" attempt(s) left"); 
+    }
+    else{
+        checkIfCredentialsAreValid(numberOfAttempts, name, password);
+    }
+}
+
+function displayMaximumAttemptsMessageAndLockSendButton(){
+    alert("You reached maximum number of attempts. Try again later"); 
+    document.getElementById("send").disabled = true;
+}
+
+function checkIfCredentialsAreValid(attempts, username, password){
+    if (username == "marcelo" && password == "goto"){
+        alert("Login successfull!");
+    }
+    else{
+        
+        numberOfAttempts--;
+        
+        if(attempts == 1){
+            displayMaximumAttemptsMessageAndLockSendButton();
+            return false;
+        }
+        
+        alert("Wrong credentials. Try again. You have "+numberOfAttempts+" attempt(s) left");
+    }
+}
